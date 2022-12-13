765a75f1d6d45da2d177f51a8165fc3503d81c7e
isroil <isroilbotiraliyev@email.com>
2022-12-13 14:16:44 +0500

    js

---
+++ main.js
@@ -0,0 +1,7 @@
+var a = +prompt('son kiriting !')
+    b= +prompt('darajani kiriting !')
+ let daraja = 1;
+    for (let i=0; i<b;i++){
+    daraja = daraja * a;
+}
+console.log( ` ${a} sonining ${b} chi darajasi ${daraja}` );
